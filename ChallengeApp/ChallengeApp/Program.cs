4d4efa42b2ca7ad67a5bea148e6767d24d40488d
AndyTaylor64 <apartnov@op.pl>
2024-02-17 00:55:26 +0100

    ZadanDoCw_analiz _liter

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,56 +1,24 @@
-﻿using ChallengeApp;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-Employee adam = new Employee("Adam", "Kowalski", 25);
-Employee monika = new("Monika", "Tokarczyk", 28);
-var damian = new Employee("Damian", "Siewkowski", 30);
-//pierwszy tydzien
-adam.AddScore(15);
-monika.AddScore(2);
-damian.AddScore(3);
-//drugi tydzien
-adam.AddScore(4);
-monika.AddScore(3);
-damian.AddScore(6);
-//trzeci tydzien
-adam.AddScore(7);
-monika.AddScore(8);
-damian.AddScore(9);
-//czwarty tydzien
-adam.AddScore(4);
-monika.AddScore(7);
-damian.AddScore(5);
-
-var resultAdam = adam.Result;
-var resultMonika = monika.Result;
-var resultDamian = damian.Result;
-
-HighResult(adam, monika, damian, resultAdam, resultMonika, resultDamian);
-
-static void HighResult(Employee adam, Employee monika, Employee damian, int resultAdam, int resultMonika, int resultDamian)
+namespace ConsoleApplication1
 {
-    List<Employee> employeeList = new List<Employee>()
-{
-    adam,
-    monika,
-    damian
-};
-
-    int highestResult = -1;
-    Employee employeeWithHiestResult = null;
-
-    foreach (var employee in employeeList)
+    class CountSymbols
     {
-        if (employee.Result > highestResult)
+        static void Main(string[] args)
         {
-            employeeWithHiestResult = employee;
-            highestResult = employee.Result;
+            Console.Write("Wprowadz dowolny ciag znakow: ");
+            var input = Console.ReadLine();
+            var groups = input.ToArray().Order().GroupBy(a => a); //Odrazu tworze tablice, sortuje i grupuje
+                                                                 
+            Console.WriteLine($"W linie:  {input}");
+
+            foreach (var num_group in groups)
+                Console.WriteLine($"Jest {num_group.Count()} znak(-i,-ow) {num_group.Key}");
+
+            Console.ReadKey();
         }
     }
-
-    Console.WriteLine("{0} {1}, {2} lat, otrzymal {3} punktow", adam.Name, adam.Surname, adam.Age, resultAdam);
-    Console.WriteLine($"{monika.Name} {monika.Surname} otrzymala {resultMonika} punktow");
-    Console.WriteLine($"{damian.Name} {damian.Surname} otrzymal " + resultDamian + " punktow");
-    Console.WriteLine();
-    Console.WriteLine(employeeWithHiestResult.Name + " " + employeeWithHiestResult.Surname + 
-                        $" jest pracownikiem miesiaca z wynikem {highestResult}");
 }
